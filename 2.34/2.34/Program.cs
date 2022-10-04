a67d24452858bcee0faec70c5cea215011a9a3a3
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2022-10-04 21:43:55 +0300

    Add files via upload

---
+++ 2.34/2.34/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        int num1, num2, num3, res1, res2;
+        Console.WriteLine("Введите число единиц двузначного слагаемого:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите число десятков двузначного слагаемого:");
+        num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите число однозначного слагаемого:");
+        num3 = int.Parse(Console.ReadLine());
+        res1 = (num1 + num3) % 10;
+        res2 = num2 + (num1 + num3) / 10;
+        Console.WriteLine($"Ответ:{res2}{res1}");
+    }
+
+}
